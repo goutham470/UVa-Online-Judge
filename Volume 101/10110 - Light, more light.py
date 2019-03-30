ff7058670cb89197c8db6c2352798e7e6fd59ee3
Goutham Kaladandi <goutham470@gmail.com>
2019-03-30 15:04:07 +0530

    Changes to 10110

--- Volume 101/10110 - Light, more light.py
+++ Volume 101/10110 - Light, more light.py
@@ -28,38 +28,61 @@ import sys
 import math
 
 
-def primefactors(n):
-    primes=[2]
-    i=2
-    while i<=math.sqrt(n):
-        if min([i%x for x in primes ])==0:
-            i=nextoddnum(i+1)
-        else:
-            primes.append(i)
-            i=i+1
-    return primes
-
+primeslst=[2,3,5,7]
 
 def nextoddnum(n):
-    if n%2==0 :
+    if n%2==0 or n%3==0 or n%5==0 or n%7==0 :
         n=nextoddnum(n+1)
     return n
 
 
+def isprime(i):
+    flag=True
+    for x in [k for k in primeslst if i%k==0 and k<=math.sqrt(i)]:
+        if i%x==0:
+            flag=False
+            break
+    return flag
+
+#nextoddnum(11+1)
+#isprime(19)
+#primefactors(76)
+#lightswitch(76)
+
+
+def primefactors(n):##Returns primes less than square root
+    i=11
+    while i<=math.sqrt(n):
+        if isprime(i):
+            primeslst.append(i)
+            i=nextoddnum(i+1)
+        else:
+            i=nextoddnum(i+1)
+    return primeslst
+
+
+
+#print(primefactors(1426539184))
+
+
 def lightswitch(n):
-    if n<=2: return "No"
-    pflst=primefactors(n)
-    factors=[]
-    for j in pflst:
-        i=j
-        while j<n: 
+    if n==1: return "Yes"
+    elif n==2 : return "No"
+    
+    pflst=[i for i in primeslst if n%i==0]
+    factors={1,n}
+    for i in pflst:
+        counter=1
+        while i<=n: 
             if n%i==0:
-                pflst.append(i)
-                factors.append(int(n/i))
+                factors.add(int(n/i))
+                factors.add(math.pow(i,counter))
+                counter+=1
                 n, i=n/i, i
             else:
                 break
-    #return factors, pflst    
+    #return factors, pflst  
+    #print(factors,pflst)
     if len(factors)%2==0:
         return "No"
     else:
@@ -71,7 +94,8 @@ if __name__ == "__main__":
     for line in sys.stdin:
         if int(line)==0 : break
         input.append(int(line))
-    [print(lightswitch(int(n))) for n in input]
+    primeslst=primefactors(max(input))
+    [print(n,lightswitch(int(n))) for n in input]
 
 #%time primefactors(1000)
 #%time lightswitch(3)
